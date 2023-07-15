05605b289801ba31c980a64398fb5fa4289babe9
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-15 19:46:35 +0900

    add shitty navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,16 @@
 
 
 export default function Navbar(){
+    return (
+        <div className={"dummy"}>
+            <div className={"navbar"}>
+                About
+                Experience
+                Projects
+                Contact
+                Resume
+            </div>
+        </div>
+    )
 
 }
