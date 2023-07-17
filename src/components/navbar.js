5b8acd5310c6ad88a8f4c67c64483f4fdf409286
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-17 20:35:30 +0900

    set rough outline for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,15 +2,16 @@
 
 export default function Navbar(){
     return (
-        <div className={"dummy"}>
-            <div className={"navbar"}>
-                About
-                Experience
-                Projects
-                Contact
-                Resume
-            </div>
-        </div>
+        <nav className={"navbar"}>
+            <ul className={"navbar-buttons"}>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Resume</li>
+
+            </ul>
+        </nav>
     )
 
 }
