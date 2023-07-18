1cf39313558c4449f189ef9a444684de7ccf3138
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-18 21:02:06 +0900

    add fixed positioning to sidebar elements and rotate right sidebar 90 degrees

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,7 +4,7 @@ import linkedin from '../assets/images/linkedin.png'
 function LeftSidebar(){
     return (
         <div className={"sidebar"}>
-            <ul>
+            <ul className={"sidebar-elements"}>
                 <li><a href={"https://github.com/lysmm203"}><img src={github}/></a></li>
                 <li><a href={"https://www.linkedin.com/in/yong-seok-lee-303185154/"}><img src={linkedin}/></a></li>
             </ul>
@@ -15,13 +15,14 @@ function LeftSidebar(){
 
 function RightSidebar(){
     return (
-        <div className={"sidebar"}>
+        <div className={"sidebar right-sidebar"}>
             <ul>
                 <li>Right sidebar</li>
                 <li>Right sidebar</li>
                 <li>Right sidebar</li>
 
             </ul>
+            {/*<p>lyeongsuk97@gmail.com</p>*/}
         </div>
     )
 }
