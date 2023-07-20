2101e8d327c4e381671afbd8f64e2a54c976e183
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-20 22:52:12 +0900

    set location for the sidebar elements and added animation when hovering

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,7 +4,7 @@ import linkedin from '../assets/images/linkedin.png'
 function LeftSidebar(){
     return (
         <div className={"sidebar"}>
-            <ul className={"sidebar-elements"}>
+            <ul className={"sidebar-elements left-sidebar"}>
                 <li><a href={"https://github.com/lysmm203"}><img src={github}/></a></li>
                 <li><a href={"https://www.linkedin.com/in/yong-seok-lee-303185154/"}><img src={linkedin}/></a></li>
             </ul>
@@ -16,7 +16,7 @@ function LeftSidebar(){
 function RightSidebar(){
     return (
         <div className={"sidebar"}>
-            <ul className={"sidebar-elements"}>
+            <ul className={"sidebar-elements right-sidebar"}>
                 <li className={"flip"}>lyeongsuk97@gmail.com</li>
             </ul>
         </div>
