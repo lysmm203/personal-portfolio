3b2dd4274ae23b2d50d8b45634a132ae774347c4
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-24 22:08:22 +0900

    add the raw content of the first section and add grid for the skill table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./components/navbar";
 import Intro from "./components/intro"
 import Body from "./components/body"
+import AboutMe from "./components/about_me"
 import {LeftSidebar, RightSidebar} from "./components/sidebar"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
               <LeftSidebar/>
               <Body>
                   <Intro/>
+                  <AboutMe/>
               </Body>
               <RightSidebar/>
           </div>
---
+++ src/components/about_me.js
@@ -0,0 +1,33 @@
+export default function AboutMe(){
+    return (
+        <div>
+            <h2>01 About Me</h2>
+            <p>Hi! My name is Tommy and I enjoy creating things with code. I first started coding in the first
+            year of college and I fell in love with it ever since.
+            </p>
+            <p>
+            Despite my late beginnings, I have had the privilege of working in various areas of computer science,
+            including <a href={"http://news-archive.anglophonechile.org/"}>NLP research</a>
+            , <a href={"https://www.line-45.com/"}>software testing</a>
+            , and <a href={"https://www.goodyear.com/"}>software development</a>.
+            These experiences have helped me apply the concepts I've learned in class to solve real-world problems,
+            and have served to deepen my interest in computer science.
+            </p>
+
+            <p>Some of the technologies I've been using are:</p>
+
+            <ul className={"skill-table"}>
+                <li className={"skill-item"}>Python</li>
+                <li className={"skill-item"}>Django</li>
+                <li className={"skill-item"}>Javascript</li>
+                <li className={"skill-item"}>React</li>
+            </ul>
+
+
+
+
+
+
+        </div>
+    )
+}
