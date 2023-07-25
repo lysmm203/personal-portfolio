882f4d12c07f367ccf5f4b3f10bcd7cb2d9601fc
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-07-25 18:39:23 +0900

    add some lorem ipsum text on intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,18 @@ export default function Intro(){
 
     return (
         <div>
-            This is the Intro Section
+            <h1>Intro Section</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec arcu sit amet augue suscipit fermentum eu et nisl. Pellentesque sit amet orci at velit condimentum vehicula. Suspendisse suscipit, mi tempor imperdiet eleifend, elit neque fermentum orci, tempor pulvinar arcu lorem in purus. Etiam ultrices dolor sit amet ullamcorper finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla consectetur at dui ut imperdiet. Nunc in ex felis. Vivamus feugiat nisl at molestie ultricies. Curabitur convallis fermentum vestibulum. Phasellus vel nisl id enim aliquam ultrices vel vitae orci. Nunc eu est mi. In felis enim, porta ac augue eget, accumsan consequat felis.
+
+                In vitae tristique diam, lobortis sodales magna. Etiam tincidunt, velit sit amet dignissim dictum, ante odio commodo orci, a gravida ex nunc ut turpis. Proin ac urna in lectus bibendum congue id non orci. Nam eleifend mauris id nunc tincidunt mattis. Nunc a erat at nulla suscipit tristique id ut lorem. Nulla lacinia mi nunc, nec molestie nulla venenatis sodales. Duis volutpat nisl dapibus tristique dictum. Suspendisse nec viverra velit. Vivamus suscipit nec turpis quis rutrum. Mauris in dictum purus, non malesuada enim. Curabitur faucibus vulputate venenatis. Suspendisse id efficitur quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris ac metus vitae ex elementum suscipit nec vitae urna. Fusce egestas ex id cursus euismod.
+
+                Duis eget nisi a leo rhoncus rhoncus id vel risus. Aenean vitae odio libero. Nullam mauris ante, viverra ac pellentesque quis, efficitur id turpis. Aenean ut sodales nisi. Nullam elementum et nisl at gravida. Nullam vitae sapien sed odio consectetur luctus. Nulla quis sem est. Sed porttitor vitae metus vitae mattis. Pellentesque nec ex ultricies eros congue pellentesque. Praesent sagittis mi odio, ac fermentum diam consectetur non. Aenean eleifend urna in vestibulum egestas. Praesent dignissim turpis nec mauris vehicula, sed sollicitudin leo vestibulum. Cras molestie euismod varius. Curabitur mollis vulputate suscipit.
+
+                Morbi eget risus est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer lobortis lectus ac enim lacinia gravida. Suspendisse non elementum diam. Ut erat tellus, dictum nec nibh id, luctus condimentum lectus. Pellentesque ac enim magna. Nam vel efficitur ex, nec ornare lacus. Phasellus ut urna sed lorem vulputate sollicitudin eu at augue. Mauris interdum, odio at pulvinar eleifend, lacus dui aliquam ex, sed finibus sapien urna sit amet lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec vel est vehicula, egestas nisl et, luctus nisi. Sed dapibus tellus a velit eleifend fringilla. Ut leo augue, mattis a blandit a, tristique sed mi. Aenean tincidunt nunc a porta molestie. Sed nunc quam, porttitor a commodo sed, commodo tincidunt enim. Quisque ultrices, sem et molestie condimentum, purus purus pellentesque tortor, in ultrices velit lorem tempor metus.
+
+
+            </p>
         </div>
     )
 
