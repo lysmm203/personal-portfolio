85ab2f1cc89b474ed541a623e92842b40d92650d
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-08-07 23:56:24 +0900

    create the array of states for each project and create the project box for each of them

--- src/components/projects/project_box.js
+++ src/components/projects/project_box.js
@@ -1,10 +1,11 @@
 import React from "react"
 
-export default function ProjectBox(){
+export default function ProjectBox(props){
     return (
         <div className={"project-box"}>
-            <h2>Project 1</h2>
-            <p>This is my project</p>
+            <h2>{props.name}</h2>
+            <h3>{props.technologies}</h3>
+            <p>{props.desc}</p>
         </div>
     )
 }
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ProjectBox from "./project_box"
 
 const projects = [
     {
@@ -27,12 +28,34 @@ const projects = [
 ]
 
 
+// Retain the design of the original projects section, but make it so that hovering over it leads to an animation where a shadow outline is drawn
+// (similar to my stock market simulator dialogue boxes) and the box moves up slightly (the moving part I might need to rethink. I'll try
+// implementing it though)
+
+
+
+
+
 export default function Projects(){
+
+
+    const [projectState, setProjectsState] = React.useState(projects)
+
+    let projectBoxes = projectState.map(project => (
+        <ProjectBox
+            name={project.name}
+            technologies={project.techonologies}
+            desc={project.desc}
+        />
+    ))
+
+
     return (
         <div className={"section"}>
             <div className={"section-heading"}>
                 <h1>03. My Projects</h1><hr className={"section-heading-line"}/>
             </div>
+            {projectBoxes}
         </div>
     )
 }
