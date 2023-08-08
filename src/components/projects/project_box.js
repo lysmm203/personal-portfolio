916313be67b65485bb788bdc87a593f985b12513
Yong Seok Lee <lyeongsuk97@gmail.com>
2023-08-08 23:36:57 +0900

    add github links to the list of projects and anchor for each project box that directs the user to the github link on a new tab

--- src/components/projects/project_box.js
+++ src/components/projects/project_box.js
@@ -2,10 +2,12 @@ import React from "react"
 
 export default function ProjectBox(props){
     return (
-        <div className={"project-box"}>
-            <h2>{props.name}</h2>
-            <h3>{props.technologies}</h3>
-            <p>{props.desc}</p>
-        </div>
+        <a href={props.link} target="_blank" rel="noopener noreferrer">
+            <div className={"project-box"}>
+                <h2>{props.name}</h2>
+                <p className={"tech-stack"}>{props.technologies}</p>
+                <p>{props.desc}</p>
+            </div>
+        </a>
     )
 }
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -5,24 +5,28 @@ const projects = [
     {
         name: "Stock Price Predictor",
         techonologies: "Python, Pandas, Keras, Matplotlib",
-        desc: "A stock prioce predictor using an LSTM to predict the historical closing stock price of the S&P500, " +
-            "NASDAQ, and DJIA indices"
+        desc: "A stock price predictor using an LSTM to predict the historical closing stock price of the S&P500, " +
+            "NASDAQ, and DJIA indices",
+        link: "https://github.com/lysmm203/stock-price-predictor"
 
     },{
         name: "Riddet",
         techonologies: "Python, Flask, MySQL, Git, Pytest",
-        desc: "A Reddit clone with basic CRUD functionalities"
+        desc: "A Reddit clone with basic CRUD functionalities",
+        link: "https://github.com/lysmm203/Riddet"
 
     },{
         name: "Stock Market Simulator",
         techonologies: "Python, Django, SQLite, Beautifulsoup, requests",
         desc: "A stock market simulator that allows users to simulate and invest in stock markets across different " +
-            "time periods"
+            "time periods",
+        link: "https://github.com/lysmm203/Stock-Market-Simulator"
 
     },{
         name: "Artket",
         techonologies: "Python, Flask, SQLAlchemy",
-        desc: "An online auction house that allows users to purchase fine-art items"
+        desc: "An online auction house that allows users to purchase fine-art items",
+        link: "https://github.com/lysmm203/Artket"
 
     }
 ]
@@ -31,10 +35,6 @@ const projects = [
 // Retain the design of the original projects section, but make it so that hovering over it leads to an animation where a shadow outline is drawn
 // (similar to my stock market simulator dialogue boxes) and the box moves up slightly (the moving part I might need to rethink. I'll try
 // implementing it though)
-
-
-
-
 
 export default function Projects(){
 
@@ -46,6 +46,7 @@ export default function Projects(){
             name={project.name}
             technologies={project.techonologies}
             desc={project.desc}
+            link={project.link}
         />
     ))
 
